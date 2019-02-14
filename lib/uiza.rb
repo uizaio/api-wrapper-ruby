f3e1ac8437949cc3b4debc40d464e645c04f25a1
NguyenThiDieu <nguyen.thi.dieu@framgia.com>
2019-02-14 09:36:15 +0700

    222473_add_storage

--- lib/uiza.rb
+++ lib/uiza.rb
@@ -5,6 +5,7 @@ require "uiza/version"
 require "uiza/uiza_client"
 
 require "uiza/api_operations/create"
+require "uiza/api_operations/add"
 require "uiza/api_operations/retrieve"
 require "uiza/api_operations/list"
 require "uiza/api_operations/update"
