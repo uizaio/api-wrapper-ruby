9e2a203554a5961cae358d6815f9e1d2984e9ba6
NguyenThiDieu <nguyen.thi.dieu@framgia.com>
2019-02-14 11:04:55 +0700

    222476_storage_remove

--- lib/uiza.rb
+++ lib/uiza.rb
@@ -9,6 +9,7 @@ require "uiza/api_operations/retrieve"
 require "uiza/api_operations/list"
 require "uiza/api_operations/update"
 require "uiza/api_operations/delete"
+require "uiza/api_operations/remove"
 
 require "uiza/entity"
 require "uiza/storage"
