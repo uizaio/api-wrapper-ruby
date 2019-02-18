6681b18e0a288152bcf4e3065dc77a241c44c525
NguyenThiDieu <nguyen.thi.dieu@framgia.com>
2019-02-18 15:54:21 +0700

    NA_category_add_api_decription_link_and_change_method_format

--- lib/uiza/category.rb
+++ lib/uiza/category.rb
@@ -4,5 +4,10 @@ module Uiza
     extend Uiza::APIOperation::Retrieve
 
     OBJECT_API_PATH = "media/metadata".freeze
+
+    OBJECT_API_DESCRIPTION_LINK = {
+      create: "https://docs.uiza.io/#create-category",
+      retrieve: "https://docs.uiza.io/#retrieve-category"
+    }.freeze
   end
 end
