d55db7e2c9b3bd934d569aed4bd0be843a12d32b
NguyenThiDieu <nguyen.thi.dieu@framgia.com>
2019-02-21 15:42:31 +0700

    224315_live_start_feed

--- lib/uiza/live.rb
+++ lib/uiza/live.rb
@@ -8,7 +8,22 @@ module Uiza
     OBJECT_API_DESCRIPTION_LINK = {
       create: "https://docs.uiza.io/#create-a-live-event",
       retrieve: "https://docs.uiza.io/#retrieve-a-live-event",
-      update: "https://docs.uiza.io/#update-a-live-event"
+      update: "https://docs.uiza.io/#update-a-live-event",
+      start_feed: "https://docs.uiza.io/#start-a-live-feed"
     }.freeze
+
+    class << self
+      def start_feed id
+        url = "https://#{Uiza.workspace_api_domain}/api/public/v3/#{OBJECT_API_PATH}/feed"
+        method = :post
+        headers = {"Authorization" => Uiza.authorization}
+        params = {id: id}
+
+        uiza_client = UizaClient.new url, method, headers, params, OBJECT_API_DESCRIPTION_LINK[:start_feed]
+        response = uiza_client.execute_request
+
+        retrieve response.id
+      end
+    end
   end
 end
