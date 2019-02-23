7af7af91e9fd4479926324697f1b498efe681532
LeTanThanh <tanthanhcr95@gmail.com>
2019-02-23 18:44:40 +0700

    NA_Fix_openstruct_bug_with_method

---
+++ lib/uiza/uiza_open_struct.rb
@@ -0,0 +1,18 @@
+module Uiza
+  class UizaOpenStruct < OpenStruct
+    def define_methods()
+      data = to_h
+      puts data
+      data.each do |key, value|
+        if value.is_a?(Uiza::UizaOpenStruct)
+          value.define_methods()
+        else
+          self.define_singleton_method(key) do |*args|
+            return super(*args) if args.any?
+            value
+          end
+        end
+      end
+    end
+  end
+end
