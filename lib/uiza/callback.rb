9160e02a6e978e98bdb980799604a0055e207cd5
NguyenThiDieu <nguyen.thi.dieu@framgia.com>
2019-02-25 15:30:22 +0700

    224858_callback_delete

--- lib/uiza/callback.rb
+++ lib/uiza/callback.rb
@@ -3,12 +3,14 @@ module Uiza
     extend Uiza::APIOperation::Create
     extend Uiza::APIOperation::Retrieve
     extend Uiza::APIOperation::Update
+    extend Uiza::APIOperation::Delete
 
     OBJECT_API_PATH = "media/entity/callback".freeze
     OBJECT_API_DESCRIPTION_LINK = {
       create: "https://docs.uiza.io/#create-a-callback",
       retrieve: "https://docs.uiza.io/#retrieve-a-callback",
-      update: "https://docs.uiza.io/#update-a-callback"
+      update: "https://docs.uiza.io/#update-a-callback",
+      delete: "https://docs.uiza.io/#delete-a-callback"
     }.freeze
   end
 end
