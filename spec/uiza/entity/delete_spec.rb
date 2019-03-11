14c14360fd6fdadecd1f3781215c837165982e71
LeTanThanh <tanthanhcr95@gmail.com>
2019-03-11 14:08:16 +0700

    228334_Ruby_v4_Entity_delete

--- spec/uiza/entity/delete_spec.rb
+++ spec/uiza/entity/delete_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 RSpec.describe Uiza::Entity do
   before(:each) do
-    Uiza.workspace_api_domain = "your-workspace-api-domain.uiza.co"
+    Uiza.app_id = "your-app-id"
     Uiza.authorization = "your-authorization"
   end
 
@@ -12,9 +12,9 @@ RSpec.describe Uiza::Entity do
         id = "your-entity-id"
 
         expected_method = :delete
-        expected_url = "https://your-workspace-api-domain.uiza.co/api/public/v3/media/entity"
+        expected_url = "https://stag-ap-southeast-1-api.uizadev.io/api/public/v4/media/entity"
         expected_headers = {"Authorization" => "your-authorization"}
-        expected_body = {id: id}
+        expected_body = {id: id, appId: "your-app-id"}
         mock_response = {
           data: {
             id: "your-entity-id"
@@ -93,9 +93,9 @@ RSpec.describe Uiza::Entity do
       id = "invalid-entity-id"
 
       expected_method = :delete
-      expected_url = "https://your-workspace-api-domain.uiza.co/api/public/v3/media/entity"
+      expected_url = "https://stag-ap-southeast-1-api.uizadev.io/api/public/v4/media/entity"
       expected_headers = {"Authorization" => "your-authorization"}
-      expected_body = {id: id}
+      expected_body = {id: id, appId: "your-app-id"}
       mock_response = {
         code: error_code,
         message: "error message"
