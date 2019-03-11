99352e67c6ced1582c953ed7c2f77fd5b258eaa0
LeTanThanh <tanthanhcr95@gmail.com>
2019-03-11 14:28:35 +0700

    228336_Ruby_V4_Entity_publish

--- lib/uiza/entity.rb
+++ lib/uiza/entity.rb
@@ -32,10 +32,10 @@ module Uiza
       end
 
       def publish id
-        url = "https://#{Uiza.workspace_api_domain}/api/public/v3/#{OBJECT_API_PATH}/publish"
+        url = "https://#{Uiza.workspace_api_domain}/api/public/#{Uiza.api_version}/#{OBJECT_API_PATH}/publish"
         method = :post
         headers = {"Authorization" => Uiza.authorization}
-        params = {id: id}
+        params = {id: id, appId: Uiza.app_id}
         description_link = OBJECT_API_DESCRIPTION_LINK[:publish]
 
         uiza_client = UizaClient.new url, method, headers, params, description_link
