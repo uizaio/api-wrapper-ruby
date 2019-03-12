ba907e5da6dbec13ed18b3b2126279f8cb1c66e5
NguyenThiDieu <nguyen.thi.dieu@framgia.com>
2019-03-12 15:00:29 +0700

    228495_v4_callback_update

--- spec/uiza/callback/update_spec.rb
+++ spec/uiza/callback/update_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 RSpec.describe Uiza::Callback do
   before(:each) do
-    Uiza.workspace_api_domain = "your-workspace-api-domain.uiza.co"
+    Uiza.app_id = "your-app-id"
     Uiza.authorization = "your-authorization"
   end
 
@@ -12,12 +12,13 @@ RSpec.describe Uiza::Callback do
         params = {
           id: "your-callback-id",
           url: "https://callback-url.uiza.co",
-          method: "GET"
+          method: "GET",
+          appId: "your-app-id"
         }
 
         # update callback
         expected_method_1 = :put
-        expected_url_1 = "https://your-workspace-api-domain.uiza.co/api/public/v3/media/entity/callback"
+        expected_url_1 = "https://stag-ap-southeast-1-api.uizadev.io/api/public/v4/media/entity/callback"
         expected_headers_1 = {"Authorization" => "your-authorization"}
         expected_body_1 = params
         mock_response_1 = {
@@ -33,9 +34,9 @@ RSpec.describe Uiza::Callback do
 
         # retrieve callback with id = "your-callback-id"
         expected_method_2 = :get
-        expected_url_2 = "https://your-workspace-api-domain.uiza.co/api/public/v3/media/entity/callback"
+        expected_url_2 = "https://stag-ap-southeast-1-api.uizadev.io/api/public/v4/media/entity/callback"
         expected_headers_2 = {"Authorization" => "your-authorization"}
-        expected_query_2 = {id: "your-callback-id"}
+        expected_query_2 = {id: "your-callback-id", appId: "your-app-id"}
         mock_response_2 = {
           data: {
             id: "your-callback-id",
@@ -116,11 +117,12 @@ RSpec.describe Uiza::Callback do
 
     def api_return_error_code error_code, error_class
       params = {
-        key: "invalid-value"
+        key: "invalid-value",
+        appId: "your-app-id"
       }
 
       expected_method = :put
-      expected_url = "https://your-workspace-api-domain.uiza.co/api/public/v3/media/entity/callback"
+      expected_url = "https://stag-ap-southeast-1-api.uizadev.io/api/public/v4/media/entity/callback"
       expected_headers = {"Authorization" => "your-authorization"}
       expected_body = params
       mock_response = {
